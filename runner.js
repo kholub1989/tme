33bc0886b2ffbc625c1d414cbe06852e28f5ec5f
Krisztian Holub <kholub1989@gmail.com>
2020-05-25 09:25:34 -0400

    adding basic reporting

--- runner.js
+++ runner.js
@@ -14,9 +14,20 @@ class Runner {
       };
       global.it = (desc, fn) => {
         beforeEaches.forEach((func) => func());
-        fn();
+        try {
+          fn();
+          console.log(`OK - ${desc}`);
+        } catch (err) {
+          console.log(`X - ${desc}`);
+          console.log('\t', err.message);
+        }
       };
-      require(file.name);
+      try {
+        require(file.name);
+      } catch (err) {
+        console.log('X - Error Loading File', file.name);
+        console.log(err.message);
+      }
     }
   }
 
