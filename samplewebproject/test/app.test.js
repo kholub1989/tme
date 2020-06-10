6f24604872a53e078eba0123d05eedfbbc8431aa
Krisztian Holub <kholub1989@gmail.com>
2020-06-09 22:41:09 -0400

    fixing a test

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -18,5 +18,18 @@ it("shows a success message with a valid email", async () => {
 
   const h1 = dom.window.document.querySelector("h1");
 
-  console.log("Contents of h1", h1.innerHTML);
+  assert.strictEqual(h1.innerHTML, "Looks good!");
+});
+
+it("shows a fail message with a invalid email", async () => {
+  const dom = await render("index.html");
+  const input = dom.window.document.querySelector("input");
+  input.value = "jsnajkajk";
+  dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+  const h1 = dom.window.document.querySelector("h1");
+
+  assert.strictEqual(h1.innerHTML, "Invalid email");
 });
